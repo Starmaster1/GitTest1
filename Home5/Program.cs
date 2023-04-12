f8e25ed7ce925346015766071898c2844915858c
Alexander Starostin <starostin.g@gmail.com>
2023-04-12 10:27:40 +0500

    Add Home5

---
+++ Home5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Home5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a = 3;
+            int b = 2;
+        }
+
+        public static int Task1(int a, int b)
+        {
+            int c = a + b;
+            return c;
+
+        }
+    }
+}
