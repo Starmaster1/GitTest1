187df269cbd77e0f86bc203e4dec6589752e68cf
Alexander Starostin <starostin.g@gmail.com>
2023-04-18 16:22:08 +0500

    Homework 6. Clinic

---
+++ Home6/Clinic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home6
+{
+    public class Clinic
+    {
+        public string Title { get; set; }
+
+        public Clinic(string title)
+        {
+            Title = title;
+        }
+        public void Registration(IlnessType ilnessType)
+        {
+            switch (ilnessType)
+            {
+                case IlnessType.Eyes:
+                    Console.WriteLine("You should got go ophthalmologist");
+                    break;
+                case IlnessType.Teeth: 
+                    Console.WriteLine("You should got go dentist");
+                    break;
+                case IlnessType.Other: 
+                    Console.WriteLine("You should got go other doctor");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Home6/Dentist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home6
+{
+    public class Dentist:Doctor
+    {
+        public Dentist(string name):base(name) {
+
+        }
+        public override void Treat(IlnessType ilness)
+        {
+
+        }
+    }
+
+}
