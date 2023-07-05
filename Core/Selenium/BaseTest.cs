2991f361aa04aa22151b7c59c85a0c945d17385d
Alexander Starostin <starostin.g@gmail.com>
2023-07-05 20:49:26 +0500

    не помню что добавил

--- Core/Selenium/BaseTest.cs
+++ Core/Selenium/BaseTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Allure.Commons;
+using NUnit.Allure.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +8,19 @@ using System.Threading.Tasks;
 
 namespace Core.Selenium
 {
+    [AllureNUnit]
     public class BaseTest
     {
+        private AllureLifecycle allure;
+
         [OneTimeSetUp]
         public void SetUp()
         {
-
+            allure = AllureLifecycle.Instance;
         }
         [TearDown] 
         public void TearDown() {
-           // Browser.Instance.CloseBrowser();
+           Browser.Instance.CloseBrowser();
         }
     }
 }
